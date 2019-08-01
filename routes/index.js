0db1f81332f1cdefd345d9ffe640340473ba5ce4
Alex <alexshipula@gmail.com>
2019-08-01 23:36:37 +0300

    changes

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,14 @@ https.get('https://api.exchangeratesapi.io/latest?base=EUR', (resp) => {
   resp.on('data', (chunk) => {
     data += chunk;
   });
-    //console.log (chunk);
+  //console.log (chunk);
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
     //console.log(JSON.parse(data));
-   // console.log(data);
-    
+    // console.log(data);
+
     convert = JSON.parse(data);
-    
+
   });
 
 }).on("error", (err) => {
@@ -26,20 +26,9 @@ https.get('https://api.exchangeratesapi.io/latest?base=EUR', (resp) => {
 //Get original transactions
 var fs = require('fs')
 var request = require('request');
-//let convert;
-//  request('https://api.exchangeratesapi.io/latest?base=EUR').pipe(
-//    fs.createWriteStream=convert
-// );
-//request('https://api.exchangeratesapi.io/latest?base=EUR').pipe()
-module.exports = router;
-console.log (convert);
+console.log(convert);
 setTimeout(() => {
-    console.log(convert);
-    
-}, 1000);
+  console.log(convert);
 
-/*router.get('/', (req, res) => {
-    res.render('index')
-})
- 
-module.exports = router*/
+}, 1000);
+module.exports = router;
